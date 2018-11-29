ff7eb7ad2363eaa4b0427869cebe23a40aba55b4
nerdylegend <31123377+nerdylegend@users.noreply.github.com>
2018-11-30 01:42:03 +0530

    Added one to one Relationship between publisher and books.

--- src/main/java/com/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.springframework.spring5webapp.bootstrap;
 
 import com.springframework.spring5webapp.model.Author;
 import com.springframework.spring5webapp.model.Book;
+import com.springframework.spring5webapp.model.Publisher;
 import com.springframework.spring5webapp.model.repositories.AuthorRepository;
 import com.springframework.spring5webapp.model.repositories.BookRepository;
+import com.springframework.spring5webapp.model.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,27 +15,38 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     private void initData() {
         //Eric
         Author eric = new Author("Eric","Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Book ddd = new Book("Domain Driven Design", "1234");
+        Publisher harper = new Publisher("Harper Collins", "#100  Apt",
+                "123 Charming Avenue", "Dallas", "Texas", "75001");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        ddd.setPublisher(harper);
+        harper.setBook(ddd);
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
 
         //George Orwell
         Author orwell = new Author("George", "Orwell");
-        Book animalFarm = new Book("Animal Farm", "4231", "Penguin Books India");
+        Book animalFarm = new Book("Animal Farm", "4231");
+        Publisher penguin = new Publisher("Penguin Books India", "#10", "Downing Street",
+                "Westminister", "London", "SW1A 2AB");
         orwell.getBooks().add(animalFarm);
         animalFarm.getAuthors().add(orwell);
+        animalFarm.setPublisher(penguin);
+        penguin.setBook(animalFarm);
+
 
         authorRepository.save(orwell);
         bookRepository.save(animalFarm);
