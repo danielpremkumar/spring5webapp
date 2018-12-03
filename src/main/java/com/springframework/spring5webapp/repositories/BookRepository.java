f52435263e1571a4afbf14f0c1ee607dcaae7117
nerdylegend <31123377+nerdylegend@users.noreply.github.com>
2018-12-04 00:48:47 +0530

    Controllers added.

--- src/main/java/com/springframework/spring5webapp/repositories/BookRepository.java
+++ src/main/java/com/springframework/spring5webapp/repositories/BookRepository.java
@@ -1,4 +1,4 @@
-package com.springframework.spring5webapp.model.repositories;
+package com.springframework.spring5webapp.repositories;
 
 import com.springframework.spring5webapp.model.Book;
 import org.springframework.data.repository.CrudRepository;
